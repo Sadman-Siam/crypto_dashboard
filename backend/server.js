3e217b795978ec1524b31615214dcc8d3eee9407
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-11-16 10:57:59 +0600

    server testing

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
-const express = require("express");
-const cors = require("cors");
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+dotenv.config();
 
+const port = process.env.PORT;
 const app = express();
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Crypto Dashboard Backend is running.");
+});
+
+app.get("/api", (req, res) => {
+  res.json({ message: "API is working", status: "success" });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
