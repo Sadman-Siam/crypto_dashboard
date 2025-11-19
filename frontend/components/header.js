6e02240520009a7ac57726c13e4943fbc18a3e57
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-11-20 00:37:45 +0600

    visual update

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -5,11 +5,11 @@ import { Input } from "./ui/input";
 
 export default function Header() {
   return (
-    <header className="bg-accent-color-1">
+    <header className="bg-accent-color-1 rounded-2xl mt-2 mx-4 hover:shadow-blue-300 shadow-md ease-in-out duration-300">
       <nav className="flex justify-between items-center p-4 ">
         <div className="flex items-center space-x-4">
           <SlGraph size={48} />
-          <h1 className="text-2xl">Crypto Graph</h1>
+          <h1 className="text-2xl font-semibold">Crypto Graph</h1>
         </div>
         <div className="flex items-center space-x-2">
           <IoMdSearch size={32} />
