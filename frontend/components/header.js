2406de04b813dc3d86b2e16ef4e4e2e46f3bdbfa
Siam Sadman <sadman.bin.anwar.siam@gmail.com>
2025-12-07 22:22:39 +0600

    update header UI

--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,21 +1,25 @@
 import { SlGraph } from "react-icons/sl";
 import { IoMdSearch } from "react-icons/io";
+import { Search } from "lucide-react";
 import { ModeToggle } from "./toggle";
 import { Input } from "./ui/input";
+import { Button } from "./ui/button";
 
 export default function Header() {
   return (
-    <header className="bg-accent-color-1 rounded-2xl mt-2 mx-4 hover:shadow-blue-300 shadow-md ease-in-out duration-300">
-      <nav className="flex justify-between items-center p-4 ">
-        <div className="flex items-center space-x-4">
-          <SlGraph size={48} />
-          <h1 className="text-2xl font-semibold">Crypto Graph</h1>
-        </div>
+    <header className="border-b-2">
+      <nav className="flex justify-between items-center px-4 py-2 ">
         <div className="flex items-center space-x-2">
-          <IoMdSearch size={32} />
-          <Input className="bg-white"></Input>
-          <ModeToggle />
+          <SlGraph size={32} />
+          <h1 className="text-lg font-semibold">Crypto Graph</h1>
         </div>
+        <div className="flex items-center space-x-1">
+          <Input className="bg-white"></Input>
+          <Button variant={`outline`}>
+            <Search size={20} />
+          </Button>
+        </div>
+        <ModeToggle />
       </nav>
     </header>
   );
